e285c6d40530f2bb74d6da985efc027af4cf1870
Naehum-Dela-Cruz <delacruznaehum@gmail.com>
2026-08-04 13:41:52 -0700

    fix (ingestion) : refactor with non-vibe coded ingestion script

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -4,20 +4,34 @@ Replace this template with your own ingestion logic.
 """
 
 import os
+import serpapi
+import json
+from datetime import datetime
+from dotenv import load_dotenv
 
+load_dotenv(os.path.join(os.path.dirname(__file__), "..", ".env"))
 RAW_DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data", "raw")
 
+dateTimeNow = datetime.now().strftime("%Y-%m-%d")
 
-def ingest():
-    # TODO: replace with your ingestion logic
-    # Examples:
-    #   - Download a CSV from a URL using requests
-    #   - Call an API and save the JSON response
-    #   - Read a manually downloaded file and copy it here
-    raise NotImplementedError("Add your ingestion logic here.")
+def primary_google_trends():
+
+    client = serpapi.Client(api_key=os.getenv('SERPAPI_API_KEY'))
+    results = client.search({
+    "engine": "google_trends",
+    "q": "masskara",
+    "data_type": "TIMESERIES",
+    "hl": "en",
+    "geo": "PH",
+    "tz": "-480",
+    "date": "now 1-d"
+    })
+
+    with open(os.path.join(RAW_DATA_DIR, f'google_trends_{dateTimeNow}.json'), 'w', encoding="utf-8") as f:
+        json.dump(results.as_dict(), f, indent=4, ensure_ascii=False)
 
 
 if __name__ == "__main__":
     os.makedirs(RAW_DATA_DIR, exist_ok=True)
-    ingest()
+    primary_google_trends()
     print("Ingestion complete. Check data/raw/ for output.")
