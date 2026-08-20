c633a060a963ad60a21405628de21769d47aa288
Naehum-Dela-Cruz <delacruznaehum@gmail.com>
2026-08-20 12:00:17 -0700

    feat (backup) : implement backup google hotel

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -27,10 +27,70 @@ def write_to_json(title, results):
     with open(os.path.join(RAW_DATA_DIR, title), 'w', encoding="utf-8") as f:
         json.dump(results.as_dict(), f, indent=4, ensure_ascii=False)
 
-def write_to_jsonl(title, results):
+def write_to_jsonl(status, primary_source, title, results):
+    data_to_write = {"status" : status, "primary_source" : primary_source, **results}
     with open(os.path.join(RAW_DATA_DIR, title), 'a', encoding="utf-8") as f:
-        f.write(json.dumps(results, ensure_ascii=False) + "\n")
+        f.write(json.dumps(data_to_write, ensure_ascii=False) + "\n")
 
+def backup_google_hotels():
+    run_input = {
+        "adults": 1,
+        "currency": "PHP",
+        "rooms": 1,
+        "searches": [
+            {
+                "location": "Bacolod",
+                "checkInDate": dateTimeNow,
+                "checkOutDate": dateTimeTommorow
+            }
+        ],
+        "sortBy": "relevance",
+        "type": "hotels"
+    }
+    
+    run = client_Apify.actor("H1scmbaCSREtaQDQU").call(run_input=run_input)
+
+    dataset_id = run.default_dataset_id
+    hasResults = False
+    if dataset_id:
+        for results in client_Apify.dataset(dataset_id).iterate_items():
+            hasResults = True
+            write_to_jsonl("success", False, f'google_hotel_{dateTimeNow}.jsonl', results)
+    if not hasResults:
+        write_to_jsonl("noResults", False, f'google_hotel_{dateTimeNow}.jsonl', 
+            {
+                "checkInDate": dateTimeNow,
+                "checkOutDate": dateTimeTommorow
+            }
+        )
+        
+def backup_google_flights():
+    run_input = {
+        "arrival_id": "BCD",
+        "currency": "PHP",
+        "departure_id": "MNL",
+        "exclude_basic": False,
+        "fetch_booking_options": False,
+        "gl": "ph",
+        "hl": "en",
+        "outbound_date": dateTimeNow,
+    }
+    
+    run = client_Apify.actor("1dYHRKkEBHBPd0JM7").call(run_input=run_input)
+
+    dataset_id = run.default_dataset_id
+    hasResults = False
+    if dataset_id:
+        for results in client_Apify.dataset(dataset_id).iterate_items():
+            hasResults = True
+            write_to_jsonl("success", False, f'google_flights_{dateTimeNow}.jsonl', results)
+    if not hasResults:
+        write_to_jsonl("noResults", False, f'google_flights_{dateTimeNow}.jsonl', 
+            {
+                "outbound_date": dateTimeNow,
+            }
+        )
+ 
 def primary_google_trends():
     results = client.search({
         "engine": "google_trends",
@@ -59,50 +119,36 @@ def primary_google_hotels():
     write_to_json(f'google_hotel_{dateTimeNow}.json', results)
 
 def primary_google_flights():
-    results = client.search({
-        "engine": "google_flights",
-        "hl": "en",
-        "gl": "ph",
-        "departure_id": "MNL",
-        "arrival_id": "BCD",
-        "outbound_date": f"{dateTimeNow}",
-        "currency": "PHP",
-        "type": "2",
-        "travel_class": "1",
-        "adults": "1",
-        "sort_by": "2"
-    })
+    try:
+        data = client.search({
+            "engine": "google_flights",
+            "hl": "en",
+            "gl": "ph",
+            "departure_id": "MNL",
+            "arrival_id": "BCD",
+            "outbound_date": dateTimeNow,
+            "currency": "PHP",
+            "type": "2",
+            "travel_class": "1",
+            "adults": "1",
+            "sort_by": "2"
+        })
 
-    write_to_json(f'google_flights_{dateTimeNow}.json', results)
+        # write_to_json(f'google_flights_{dateTimeNow}.json', results)
 
-def backup_google_hotels():
-    run_input = {
-        "adults": 1,
-        "currency": "PHP",
-        "ecoCertified": False,
-        "freeCancellation": False,
-        "includeDetails": False,
-        "includeReviews": False,
-        "rooms": 1,
-        "searches": [
-            {
-                "location": "Bacolod",
-                "checkInDate": dateTimeTommorow,
-                "checkOutDate": dateTimeTommorow2
-            }
-        ],
-        "sortBy": "relevance",
-        "specialOffers": False,
-        "type": "hotels"
-    }
-    
-    run = client_Apify.actor("H1scmbaCSREtaQDQU").call(run_input=run_input)
+        if not "error" in data:
+            for results in data:   
+                write_to_jsonl("success", True, f'google_flights_{dateTimeNow}.jsonl', results)
+        else:
+            write_to_jsonl("noResults", True, f'google_flights_{dateTimeNow}.jsonl', 
+                {
+                    "outbound_date": dateTimeNow,
+                }
+            )
+    except:
+        backup_google_flights()
 
-    dataset_id = run.default_dataset_id
-    if dataset_id:
-        for results in client_Apify.dataset(dataset_id).iterate_items():
-            write_to_jsonl(f'google_hotel_{dateTimeNow}_backup.jsonl', results)
- 
+        
 
 
 if __name__ == "__main__":
@@ -111,5 +157,6 @@ if __name__ == "__main__":
     # primary_google_trends()
     # primary_google_hotels()
     # primary_google_flights()
-    backup_google_hotels()
+    # backup_google_hotels()
+    backup_google_flights()
     print("Ingestion complete. Check data/raw/ for output.")
